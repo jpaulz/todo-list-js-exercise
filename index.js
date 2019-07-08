af5914a1c5bf4bf80e611275c154c5e03be3b159
Julia Paulasaari <julia@paulasaari.com>
2019-07-08 02:04:00 +0000

    Added ToDo List assignment

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// // Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescription = [];
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+// // Create a new task by adding to the arrays
+// // A new task will be created as incomplete
+// function newTask(title, description) {
+//   taskTitles.push(title);
+//   taskDescription.push(description);
+//   taskComplete.push(false);
+// }
+const newTask = function(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+
+  };
+  return task;
+};
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
+// function completeTask(taskIndex) {
+//   taskComplete[taskIndex] = true;
+// }
+// marks the provided task as completed
+const completeTask = function(task) {
+  task.complete = true;
+};
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+// function logTaskState(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// }
+// prints out the provided task's details
+const logTaskState = function(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+};
+
+
 
 // DRIVER CODE BELOW
 
@@ -29,3 +56,16 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+// logTaskState(task1); // Clean Cat Litter has not been completed
+// completeTask(task1);
+// logTaskState(task1); // Clean Cat Litter has been completed
+
+// console.log(tasks);
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
